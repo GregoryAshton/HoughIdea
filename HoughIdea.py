0ada75b7c70d70d9ef98a458d8991113256eb7d0
Gregory Ashton <gregory.ashton@ligo.org>
2014-06-16 11:48:43 +0100

    Adds peak finding plots the results and puts things into functions

--- HoughIdea.py
+++ HoughIdea.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import ndimage
 
 x = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30])
 y = np.array([2,5,7,9,10,13,16,18,21,22,21,20,19,18,17,14,10,9,7,5,7,9,10,12,13,15,16,17,22,27])
@@ -11,34 +12,91 @@ def GetMaximum(array):
     i, j = np.unravel_index(array.argmax(), array.shape)
     return i, j
 
-nM, nC = 500, 510
-m = np.linspace(-2, 4, nM)
-c = np.linspace(-35, 15, nC)
+def GetMCArray(x, y, m, c, threshold=2):
+    """ Returns the Hough transformed array of x, y data
 
-M, C = np.meshgrid(m, c)
+    Parameters
+    ----------
+    x : array_like 
+        The x data of length *n*
+    y : array_like
+        The y data of length *n*
+    m : array_like
+        1Dcarray of gradient values 
+    c : array_like
+        1D array of c values
 
-bins = [] 
-threshold = 1.0
-for xval, yval in zip(x, y):
-    residual = np.abs(yval - (M*xval + C))
-    bins.append((residual < threshold).astype(int))
+    Returns
+    -------
+    bins : array, shape(n, len(m), len(c))
 
-bins = np.array(bins)
-bins_count = bins.sum(axis=0)
-fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(8,4))
+    """
 
-ax1.plot(x, y)
+    M, C = np.meshgrid(m, c, indexing="ij")
+    bins = [] 
+    for xval, yval in zip(x, y):
+        residual = np.abs(yval - (M*xval + C))
+        bins.append((residual < threshold).astype(int))
 
-cplt = ax2.pcolormesh(M, C, bins_count)
-ax2.set_xlabel("m")
-ax2.set_ylabel("c")
-plt.colorbar(cplt)
+    bins = np.array(bins)
+    return M, C, bins
 
-i, j = GetMaximum(bins_count)
-x_fit = x[bins[:, i, j] == 1]
-ax1.plot(x_fit, M[i, j]*x_fit + C[i, j])
+def GetMCMaximums(bins, size):
+    """ Return the indexes of the maximum values
 
-plt.show()
+    Parameters
+    ----------
+    bins : array, 
+
+    size : int
 
 
-plt.show()
+    Returns
+    -------
+    i_idxs : array
+        Indexes of the peaks in m
+    j_idxs : array
+        Indexes of the peaks in c
+    """
+    maxima = (bins_count == ndimage.maximum_filter(bins_count, 150))
+    i_idxs, j_idxs = np.nonzero(maxima)
+
+    return i_idxs, j_idxs
+
+if __name__ == "__main__":
+
+    nM, nC = 200, 201
+    size = 10
+    threshold = 1.0
+
+    m = np.linspace(-5, 5, nM)
+    c = np.linspace(-200, 100, nC)
+    M, C, bins = GetMCArray(x, y, m, c, threshold=threshold)
+    bins_count = bins.sum(axis=0)
+
+
+
+    fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(8,4))
+
+    ax1.plot(x, y)
+
+    cplt = ax2.pcolormesh(M, C, bins_count)
+    ax2.set_xlabel("m")
+    ax2.set_ylabel("c")
+    plt.colorbar(cplt)
+
+    i_idxs, j_idxs = GetMCMaximums(bins, size=size)
+
+    #if len(i_idxs) > 100:
+    #    raise RuntimeError(" I don't want to plot {} fits".format(len(i_idxs)))
+
+    for i, j in zip(i_idxs, j_idxs):
+        x_fit = x[bins[:, i, j] == 1]
+        ax1.plot(x_fit, m[i]*x_fit + c[j], alpha=0.8, color="r")
+
+    plt.show()
+
+
+
+
+
